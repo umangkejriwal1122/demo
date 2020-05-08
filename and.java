894d4059744cbff1fbd1d24752d5d7eac2218679
umangkejriwal1122 <info.apptronix@gmail.com>
2020-05-08 09:56:51 +0530

    Update and.java

--- and.java
+++ and.java
@@ -1,7 +1,10 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -24,7 +27,7 @@ public class Main2Activity extends AppCompatActivity {
     MyAdapter adapter;
     ArrayList<String> states;
     String url = "https://tum-auditor.000webhostapp.com/get.php";
-    ArrayList<String> states_array;
+    ArrayList<String> states_array,con_array,death_array,rec_array,act_array;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,18 @@ public class Main2Activity extends AppCompatActivity {
         lv = findViewById(R.id.list);
         states = new ArrayList<String>();
         getData();
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView tv = view.findViewById(R.id.textView);
+                String name = tv.getText().toString();
+                String confirmed = con_array.get(i);
+                String recovered = rec_array.get(i);
+                String deaths = death_array.get(i);
+                String active = act_array.get(i);
+                Toast.makeText(Main2Activity.this, confirmed, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void getData() {
@@ -45,10 +60,22 @@ public class Main2Activity extends AppCompatActivity {
                     JSONObject jo = new JSONObject(response);
                     JSONArray ja =jo.getJSONArray("covid");
                     states_array = new ArrayList<String>();
+                    con_array = new ArrayList<String>();
+                    death_array = new ArrayList<String>();
+                    rec_array = new ArrayList<String>();
+                    act_array = new ArrayList<String>();
                     for (int i=0;i< ja.length();i++){
                         JSONObject json_object = ja.getJSONObject(i);
                         String state_name = json_object.getString("states");
+                        String c = json_object.getString("confirmed");
+                        String r = json_object.getString("recovered");
+                        String d = json_object.getString("deaths");
+                        String a = json_object.getString("active");
                         states_array.add(state_name);
+                        con_array.add(c);
+                        rec_array.add(r);
+                        death_array.add(d);
+                        act_array.add(a);
                     }
                 //    Toast.makeText(Main2Activity.this, ""+states_array, Toast.LENGTH_SHORT).show();
                     adapter = new MyAdapter(Main2Activity.this,states_array);
