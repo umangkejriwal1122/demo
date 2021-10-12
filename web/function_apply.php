0aca5ac9aea66cb6abce7f0dade3db0e3776c4a9
Umang Kejriwal <info.apptronix@gmail.com>
2021-10-12 13:58:10 +0530

    Delete web directory

--- web/function_apply.php
+++
@@ -1,9 +0,0 @@
-<title>Functions</title>
-<?php
-#include("functions.php");  // return warning if there is error in file 
-require("functions.php");  // return error if there is error in file 
-add(10,30);
-
-write_my_file("umang.txt","My name is Umang");
-
-?>
